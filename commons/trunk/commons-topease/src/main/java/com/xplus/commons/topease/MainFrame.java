2caa6e5d5ec8725b3e87e8c03a6e4b28d30b9dfa
hunnyhu <huxiong888@163.com>
2017-05-07 22:20:20 +0800

    swing test

---
+++ commons/trunk/commons-topease/src/main/java/com/xplus/commons/topease/MainFrame.java
@@ -0,0 +1,37 @@
+package com.xplus.commons.topease;
+
+import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.plaf.metal.MetalLookAndFeel;
+
+import org.springframework.stereotype.Component;
+
+@Component(MainFrame.BEAN_ID)
+public class MainFrame extends JFrame {
+	
+	public static final String BEAN_ID = "commons-topease.swing.frame.MainFrame";
+
+	private static final long serialVersionUID = 3976209541974109976L;
+
+	private JMenuBar mBar = new JMenuBar();
+	
+	public MainFrame() {
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		this.setLocationByPlatform(true);
+		this.setJMenuBar(mBar);
+	}
+	
+  public static void changeLookAndFeel(JFrame frame) {
+    try {
+      UIManager.setLookAndFeel(new MetalLookAndFeel());
+    } catch (UnsupportedLookAndFeelException e1) {
+      e1.printStackTrace();
+    }
+    SwingUtilities.updateComponentTreeUI(frame);
+    frame.pack();
+  }
+
+}

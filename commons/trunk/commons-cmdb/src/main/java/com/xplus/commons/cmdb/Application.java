3fea2c976b9a7e7313681e08ddf7bbfae9c45264
huzexiong <huxiong888@163.com>
2017-07-05 20:06:56 +0800

    Optimize.

--- commons/trunk/commons-cmdb/src/main/java/com/xplus/commons/cmdb/Application.java
+++ commons/trunk/commons-cmdb/src/main/java/com/xplus/commons/cmdb/Application.java
@@ -2,8 +2,10 @@ package com.xplus.commons.cmdb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:commons-cmdb.properties")
 public class Application {
 
   public static void main(String[] args) {
--- commons/trunk/commons-cmdb/src/main/java/com/xplus/commons/cmdb/web/CmdbController.java
+++ commons/trunk/commons-cmdb/src/main/java/com/xplus/commons/cmdb/web/CmdbController.java
@@ -1,5 +1,8 @@
 package com.xplus.commons.cmdb.web;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,11 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/stack")
 public class CmdbController {
 
+  private static final Map<String, String> URLS = new HashMap<String, String>();
+  
+  static {
+    URLS.put("dpos", "http://localhost:8082/rs/");
+    URLS.put("mbr", "http://localhost:8080/web/");
+    URLS.put("prepay", "http://localhost:8080/rs/");
+  }
+  
   @RequestMapping(value = "/get/{app}/{requestid}", method = RequestMethod.GET)
   public Stack get(@PathVariable String app, //
       @PathVariable String requestid) {
     Stack stack = new Stack();
-    stack.setServerUrl("http://localhost:8080/test/");
+    stack.setServerUrl(URLS.get(app));
     return stack;
   }
 

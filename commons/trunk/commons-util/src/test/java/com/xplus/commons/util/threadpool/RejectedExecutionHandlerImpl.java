dcca56773b449d86afe3feb655ceabaeba539895
hunnyhu <huxiong888@163.com>
2017-09-28 00:31:54 +0800

    Thread Pool.

---
+++ commons/trunk/commons-util/src/test/java/com/xplus/commons/util/threadpool/RejectedExecutionHandlerImpl.java
@@ -0,0 +1,13 @@
+package com.xplus.commons.util.threadpool;
+
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class RejectedExecutionHandlerImpl implements RejectedExecutionHandler {
+
+  @Override
+  public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+    System.out.println(r.toString() + " is rejected");
+  }
+
+}

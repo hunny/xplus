fac0f552919fea56c99ad88e59c58e159c03d2ba
hunnyhu <huxiong888@163.com>
2017-10-07 21:30:51 +0800

    Router zuul.

---
+++ server/trunk/service-zuul/src/main/java/com/xplus/server/service/zuul/Application.java
@@ -0,0 +1,17 @@
+package com.xplus.server.service.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}

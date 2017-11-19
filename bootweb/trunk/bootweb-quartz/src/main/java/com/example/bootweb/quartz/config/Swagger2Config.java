231e7fe4a6e2bb6008fc31da2bbb54a40b2d09f1
hunnyhu <huxiong888@163.com>
2017-11-19 21:47:56 +0800

    Add Swagger2.

---
+++ bootweb/trunk/bootweb-quartz/src/main/java/com/example/bootweb/quartz/config/Swagger2Config.java
@@ -0,0 +1,13 @@
+package com.example.bootweb.quartz.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import com.example.bootweb.swagger.EnableSwagger2Api;
+
+@Configuration
+@EnableSwagger2Api
+public class Swagger2Config {
+
+  // Do Nothing.
+
+}

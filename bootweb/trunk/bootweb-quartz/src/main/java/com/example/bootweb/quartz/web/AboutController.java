19f70f631d99d0ee939899d589a059057d2e37eb
hunnyhu <huxiong888@163.com>
2017-11-19 16:07:04 +0800

    Get Spring Active Profiles.

--- bootweb/trunk/bootweb-quartz/src/main/java/com/example/bootweb/quartz/web/AboutController.java
+++ bootweb/trunk/bootweb-quartz/src/main/java/com/example/bootweb/quartz/web/AboutController.java
@@ -1,6 +1,10 @@
 package com.example.bootweb.quartz.web;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +16,24 @@ public class AboutController {
 
   @Autowired
   private AboutService aboutService;
-  
-  @RequestMapping(value = {"/about"}, method = RequestMethod.GET)
+
+  @Autowired
+  private Environment environment;
+
+  @RequestMapping(value = { "/about" }, method = RequestMethod.GET)
   public String about() {
     return aboutService.about();
   }
-  
+
+  /**
+   * 参考[集成maven和Spring
+   * boot的profile功能]{@link http://blog.csdn.net/lihe2008125/article/details/50443491}
+   * 
+   * @return list
+   */
+  @RequestMapping(value = { "/active/profiles" }, method = RequestMethod.GET)
+  public List<String> profile() {
+    return Arrays.asList(environment.getActiveProfiles());
+  }
+
 }

6511551a1a35211188df95ddfa4628bea171b1e1
huzexiong <huxiong888@163.com>
2017-11-21 12:55:45 +0800

    Add Runner.

--- springboot-spark/trunk/spark-starter/src/main/java/com/springboot/spark/starter/Application.java
+++ springboot-spark/trunk/spark-starter/src/main/java/com/springboot/spark/starter/Application.java
@@ -1,10 +1,16 @@
 package com.springboot.spark.starter;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+  @Override
+  public void run(String... args) throws Exception {
+    System.out.println("Run starter inside.");
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);

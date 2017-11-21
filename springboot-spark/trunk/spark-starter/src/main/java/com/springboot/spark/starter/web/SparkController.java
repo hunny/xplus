2abac489b0db2c4eaac12381b6fe7e10619df267
huzexiong <huxiong888@163.com>
2017-11-21 16:20:23 +0800

    Refactor Project With logger.

--- springboot-spark/trunk/spark-starter/src/main/java/com/springboot/spark/starter/web/SparkController.java
+++ springboot-spark/trunk/spark-starter/src/main/java/com/springboot/spark/starter/web/SparkController.java
@@ -1,19 +1,24 @@
 package com.springboot.spark.starter.web;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.spark.starter.profile.SparkWordCount;
 import com.springboot.spark.starter.service.Count;
 import com.springboot.spark.starter.service.WordCountSparkService;
 
-@RequestMapping("/spark")
-@Controller
+@RequestMapping(value = "/spark", //
+    produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+@RestController
 @SparkWordCount
 public class SparkController {
 
@@ -21,8 +26,17 @@ public class SparkController {
   private WordCountSparkService wordCountSparkService;
 
   @RequestMapping("/word/count")
+  @SparkWordCount
   public ResponseEntity<List<Count>> wordCount() {
-      return new ResponseEntity<>(wordCountSparkService.count(), HttpStatus.OK);
+    return new ResponseEntity<>(wordCountSparkService.count(), HttpStatus.OK);
   }
-  
+
+  @GetMapping("/about")
+  public ResponseEntity<Map<String, String>> about() {
+    Map<String, String> map = new HashMap<String, String>();
+    map.put("application.name", "spark sample.");
+    map.put("application.version", "0.0.1-SNAPSHOT");
+    return new ResponseEntity<>(map, HttpStatus.OK);
+  }
+
 }

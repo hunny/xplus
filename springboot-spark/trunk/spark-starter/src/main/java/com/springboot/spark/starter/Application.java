1203e9b5de00cead9b4558283c541d8adec0fc23
huzexiong <huxiong888@163.com>
2017-11-21 12:56:06 +0800

    Remove Runner.

--- springboot-spark/trunk/spark-starter/src/main/java/com/springboot/spark/starter/Application.java
+++ springboot-spark/trunk/spark-starter/src/main/java/com/springboot/spark/starter/Application.java
@@ -1,16 +1,10 @@
 package com.springboot.spark.starter;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
-
-  @Override
-  public void run(String... args) throws Exception {
-    System.out.println("Run starter inside.");
-  }
+public class Application {
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);

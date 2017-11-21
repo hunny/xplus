5f099a8d64b9c5bff0803393469d0dba0fc6f348
hunnyhu <huxiong888@163.com>
2017-11-21 21:29:53 +0800

    optimize code.

--- bootweb/trunk/bootweb-angularjs/src/main/java/com/example/bootweb/angularjs/service/MarkdownService.java
+++ bootweb/trunk/bootweb-angularjs/src/main/java/com/example/bootweb/angularjs/service/MarkdownService.java
@@ -5,7 +5,6 @@ import java.io.FileFilter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;

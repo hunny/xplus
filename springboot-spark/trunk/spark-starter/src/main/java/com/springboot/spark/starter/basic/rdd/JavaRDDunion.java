64a0890b44fe2d8b2465d16bfdbc7c6b02b0d361
huzexiong <huxiong888@163.com>
2017-12-11 11:19:36 +0800

    JavaRDD union.

---
+++ springboot-spark/trunk/spark-starter/src/main/java/com/springboot/spark/starter/basic/rdd/JavaRDDunion.java
@@ -0,0 +1,36 @@
+package com.springboot.spark.starter.basic.rdd;
+
+import java.io.Serializable;
+import java.util.Arrays;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+/**
+ * 
+ */
+public class JavaRDDunion implements Serializable {
+
+  private static final long serialVersionUID = 5924321403879655951L;
+
+  private final static SparkConf sparkConf = new SparkConf() //
+      .setAppName("JavaRDDunion") //
+      .setMaster("local") //
+  ; //
+
+  public final static JavaSparkContext sc = new JavaSparkContext(sparkConf);
+
+  public void usingUnion() {
+    JavaRDD<Integer> rdd1 = sc.parallelize(Arrays.asList(1, 2, 3, 4, 5));
+    JavaRDD<Integer> rdd2 = sc.parallelize(Arrays.asList(5, 6, 7, 8, 9, 10));
+    JavaRDD<Integer> rdd3 = rdd1.union(rdd2);
+    System.out.println("大小：" + rdd3.count() + ", 数据：" + rdd3.collect());
+  }
+
+  public static void main(String[] args) {
+    new JavaRDDunion().usingUnion();
+    sc.close();
+  }
+
+}

82289d29998ed5bddc3bcfd740ecb083e8ae5a2f
huzexiong <huxiong888@163.com>
2017-12-11 11:25:16 +0800

    intersection.

--- springboot-spark/trunk/spark-starter/src/main/java/com/springboot/spark/starter/basic/rdd/JavaRDDintersection.java
+++ springboot-spark/trunk/spark-starter/src/main/java/com/springboot/spark/starter/basic/rdd/JavaRDDintersection.java
@@ -10,27 +10,27 @@ import org.apache.spark.api.java.JavaSparkContext;
 /**
  * 
  */
-public class JavaRDDunion implements Serializable {
+public class JavaRDDintersection implements Serializable {
 
   private static final long serialVersionUID = 5924321403879655951L;
 
   private final static SparkConf sparkConf = new SparkConf() //
-      .setAppName("JavaRDDunion") //
+      .setAppName("JavaRDDintersection") //
       .setMaster("local") //
   ; //
 
   public final static JavaSparkContext sc = new JavaSparkContext(sparkConf);
 
-  public void usingUnion() {
+  public void usingIntersection() {
     JavaRDD<Integer> rdd1 = sc.parallelize(Arrays.asList(1, 2, 3, 4, 5));
     JavaRDD<Integer> rdd2 = sc.parallelize(Arrays.asList(5, 6, 7, 8, 9, 10));
-    // 将两个RDD中的数据集进行合并，最终返回两个RDD的并集，若RDD中存在相同的元素也不会去重
-    JavaRDD<Integer> rdd3 = rdd1.union(rdd2);
+    // 返回两个RDD的交集
+    JavaRDD<Integer> rdd3 = rdd1.intersection(rdd2);
     System.out.println("大小：" + rdd3.count() + ", 数据：" + rdd3.collect());
   }
 
   public static void main(String[] args) {
-    new JavaRDDunion().usingUnion();
+    new JavaRDDintersection().usingIntersection();
     sc.close();
   }
 

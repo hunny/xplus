fd2e0e93810b4611b8e3d9d704d71d385d0e9eca
huzexiong <huxiong888@163.com>
2018-01-09 18:28:22 +0800

    cors security.

---
+++ springboot/trunk/example-actuator-custom-security/src/test/java/com/example/bootweb/actuator/security/CorsSampleActuatorApplicationTests.java
@@ -0,0 +1,73 @@
+package com.example.bootweb.actuator.security;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.net.URI;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.LocalHostUriTemplateHandler;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.context.ApplicationContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+import com.example.springboot.actuator.security.Application;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class, //
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext
+@ActiveProfiles("cors")
+public class CorsSampleActuatorApplicationTests {
+
+  private TestRestTemplate testRestTemplate;
+
+  @Autowired
+  private ApplicationContext applicationContext;
+
+  @Before
+  public void setUp() throws Exception {
+    RestTemplate restTemplate = new RestTemplate();
+    LocalHostUriTemplateHandler handler = new LocalHostUriTemplateHandler(
+        this.applicationContext.getEnvironment(), "http");
+    restTemplate.setUriTemplateHandler(handler);
+    restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
+    this.testRestTemplate = new TestRestTemplate(restTemplate);
+  }
+
+  @Test
+  public void endpointShouldReturnUnauthorized() throws Exception {
+    ResponseEntity<?> entity = this.testRestTemplate.getForEntity("/env", Map.class);
+    assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
+  }
+
+  @Test
+  public void preflightRequestToEndpointShouldReturnOk() throws Exception {
+    RequestEntity<?> healthRequest = RequestEntity.options(new URI("/env"))
+        .header("Origin", "http://localhost:8080").header("Access-Control-Request-Method", "GET")
+        .build();
+    ResponseEntity<?> exchange = this.testRestTemplate.exchange(healthRequest, Map.class);
+    assertThat(exchange.getStatusCode()).isEqualTo(HttpStatus.OK);
+  }
+
+  @Test
+  public void preflightRequestWhenCorsConfigInvalidShouldReturnForbidden() throws Exception {
+    RequestEntity<?> entity = RequestEntity.options(new URI("/env"))
+        .header("Origin", "http://localhost:9095").header("Access-Control-Request-Method", "GET")
+        .build();
+    ResponseEntity<byte[]> exchange = this.testRestTemplate.exchange(entity, byte[].class);
+    assertThat(exchange.getStatusCode()).isEqualTo(HttpStatus.FORBIDDEN);
+  }
+
+}

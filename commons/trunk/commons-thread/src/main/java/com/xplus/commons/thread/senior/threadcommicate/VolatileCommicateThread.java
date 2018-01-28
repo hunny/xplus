a1c8ee2785c2b9a6a66cd214252cb5ac5e122f88
hunnyhu <huxiong888@163.com>
2018-01-28 19:27:07 +0800

    wait notify.

--- commons/trunk/commons-thread/src/main/java/com/xplus/commons/thread/senior/threadcommicate/VolatileCommicateThread.java
+++ commons/trunk/commons-thread/src/main/java/com/xplus/commons/thread/senior/threadcommicate/VolatileCommicateThread.java
@@ -1,15 +1,15 @@
-package com.xplus.commons.thread.senior.lock.case1;
+package com.xplus.commons.thread.senior.threadcommicate;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 /**
- * 演示volatile关键字的使用。
+ * 演示volatile关键字的使用及线程间的通信。
  */
-public class DemoVolatileThread {
+public class VolatileCommicateThread {
 
-  // list使用volatile修饰，使多个线程实时都可见。
+  // list使用volatile修饰，使多个线程实时都可见，可实现线程间通信。
   private volatile List<String> list = new ArrayList<>();
 
   public void add(String value) {
@@ -21,7 +21,7 @@ public class DemoVolatileThread {
   }
 
   public static void main(String[] args) {
-    final DemoVolatileThread demo = new DemoVolatileThread();
+    final VolatileCommicateThread demo = new VolatileCommicateThread();
     Thread thread1 = new Thread() {
       @Override
       public void run() {
---
+++ commons/trunk/commons-thread/src/main/java/com/xplus/commons/thread/senior/threadcommicate/WaitNotifyCommicateThread.java
@@ -0,0 +1,81 @@
+package com.xplus.commons.thread.senior.threadcommicate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * 演示使用wait/notify来进行线程间的通信。
+ */
+public class WaitNotifyCommicateThread {
+
+  private volatile List<String> list = new ArrayList<>();
+
+  public void add(String value) {
+    list.add(value);
+  }
+
+  public int getSize() {
+    return list.size();
+  }
+
+  public static void main(String[] args) {
+
+    final WaitNotifyCommicateThread demo = new WaitNotifyCommicateThread();
+
+    final Object lock = new Object();
+
+    Thread thread1 = new Thread(new Runnable() {
+      @Override
+      public void run() {
+        synchronized (lock) {
+          for (int i = 0; i < 10; i++) {
+            String value = String.valueOf(new Random().nextInt(100));
+            demo.add(value);
+            System.out.println(value);
+            try {
+              Thread.sleep(500);
+            } catch (InterruptedException e) {
+              e.printStackTrace();
+            }
+            if (demo.getSize() == 5) {
+              System.out.println("当前线程" //
+                  + Thread.currentThread().getName() //
+                  + "已经发出通知。" //
+              );
+              lock.notify();// 不释放锁
+            }
+          }
+        }
+      }
+    }, "t1");
+
+    Thread thread2 = new Thread(new Runnable() {
+      @Override
+      public void run() {
+        synchronized (lock) {
+          if (demo.getSize() != 5) {
+            try {
+              System.out.println(Thread.currentThread().getName() + "进入。");
+              lock.wait();// 释放锁
+            } catch (InterruptedException e) {
+              e.printStackTrace();
+            }
+          }
+          System.out.println( //
+              "当前线程" //
+                  + Thread.currentThread().getName() //
+                  + "收到通知，值为" //
+                  + demo.getSize() //
+                  + "，线程停止。" //
+          );
+          throw new RuntimeException("线程停止。");
+        }
+      }
+    }, "t2");
+
+    thread2.start();
+    thread1.start();
+  }
+
+}

d030382fdfc423344fb52c6ffc3ff6f1431fcf8d
hunnyhu <huxiong888@163.com>
2018-01-30 22:57:57 +0800

    add queue.

--- commons/trunk/commons-thread/src/main/java/com/xplus/commons/thread/senior/queue/UseQueue.java
+++ commons/trunk/commons-thread/src/main/java/com/xplus/commons/thread/senior/queue/UseQueue.java
@@ -1,14 +1,24 @@
 package com.xplus.commons.thread.senior.queue;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.TimeUnit;
 
 public class UseQueue {
 
   public static void main(String[] args) {
-    useLinkedQueue();
+//    useConcurrentLinkedQueue();
+//    useArrayBlockingQueue();
+    useLinkedBlockingQueue();
+    useSynchronousQueue();
   }
   
-  public static void useLinkedQueue() {
+  public static void useConcurrentLinkedQueue() {
+    System.out.println("===============useConcurrentLinkedQueue");
     // 高性能无阻塞无界队列
     ConcurrentLinkedQueue<String> q = new ConcurrentLinkedQueue<>();
     q.offer("a");
@@ -18,6 +28,91 @@ public class UseQueue {
     System.out.println(q.size());
     System.out.println(q.peek());
     System.out.println(q.size());
+  }
+  
+  public static void useArrayBlockingQueue() {
+    System.out.println("===============useArrayBlockingQueue");
+    final ArrayBlockingQueue<String> q = new ArrayBlockingQueue<>(5);
+    q.add("a");
+    try {
+      q.put("b");// wait for space to become available if the queue is full.
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    q.offer("c");
+    q.add("d");
+    q.add("e");
+    try {
+      q.add("e");
+    } catch (Exception e) {
+      System.out.println("可预见的Queue满异常：" + e.getMessage());
+    }
+    try {
+      q.offer("g", 1, TimeUnit.SECONDS);
+    } catch (InterruptedException e) {
+      System.out.println("可预见的Queue超时异常：" + e.getMessage());
+    }
+    System.out.println("Queue中的元素为：" + q);
+    new Thread(new Runnable() {
+      @Override
+      public void run() {
+        try {
+          System.out.println("等待被拿走");
+          Thread.sleep(500);
+          System.out.println("已经拿走元素：" + q.take());// 拿走第一个元素
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      }
+    }).start();
+    try {
+      //准备放入元素
+      System.err.println("准备放入元素。");
+      q.put("g");// wait for space to become available if the queue is full.
+      System.err.println("放入元素完成。");
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    try {
+      Thread.sleep(1000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    System.out.println("变动后元素为Queue中的元素为：" + q);
+  }
+  
+  public static void useLinkedBlockingQueue() {
+    System.out.println("===============useLinkedBlockingQueue");
+    LinkedBlockingQueue<String> q = new LinkedBlockingQueue<>(2);
+    q.add("1");
+    try {
+      q.put("2");
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    q.offer("3");
+    try {
+      q.add("4");
+    } catch (Exception e) {
+      System.out.println("Queue满的异常：" + e.getMessage());
+    }
+    System.out.println("Queue大小：" + q.size());
+    List<String> list = new ArrayList<>();
+    System.out.println("元素个数:" + q.drainTo(list, 4));
+    System.out.println("大小：" + list.size());
+    for (String str : list) {
+      System.out.println("元素：" + str);
+    }
+  }
+  
+  public static void useSynchronousQueue() {
+    System.out.println("===============useSynchronousQueue");
+    SynchronousQueue<String> q = new SynchronousQueue<>();
+    try {
+      q.add("12345");
+    } catch (Exception e) {
+      System.out.println("可预见的异常：" + e.getMessage());
+    }
   }
 
 }
--- commons/trunk/commons-thread/src/main/java/com/xplus/commons/thread/senior/queue/并发Queue.md
+++ commons/trunk/commons-thread/src/main/java/com/xplus/commons/thread/senior/queue/并发Queue.md
@@ -16,14 +16,19 @@ poll()和peek()都是取元素节点，区别在于poll()会删除元素。
 
 ### LinkedBlockingQueue
 
-12
 基于链表的阻塞队列，同ArrayBlockingQueue类似，其内部也维持着一个数据缓冲队列（该队列由一个链表构成），LinkedBlockingQueue之所以能够高效的处理并发数据，是因为其内部实现采用分离锁（读写分离两个锁），从而实现生产者和消费者操作的完全并行运行，它是一个无界队列。
 
 ### PriorityBlockingQueue
 
+基于优先级的阻塞队列（优先级的判断通过构造函数传入的Compator对象来决定，也就是说传入队列的对象必须实现Comparable接口），在实现PriorityBlockingQueue时，内部控制线程同步的锁来用的是公平锁，它也是一个无界的队列。
+
 ### DelayQueue
 
+带有延迟时间的Queue，其中的元素只有当其指定的延迟时间到了，才能够从队列中获取到该元素。DelayQueue中的元素必须实现Delayed接口，DelayQueue是一个没有大小限制的队列，应用场景很多，比如对缓存超时的数据进行移除，任务超时处理、空闲连接的关闭等等。
+
 ### SynchronizedQueue
+
+一种没有缓冲的队列，生产者产生的数据直接会被消费者获取并消费。
 
 
 

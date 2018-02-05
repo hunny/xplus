d81795b01ef58891a9a0fbf334002bdcd6dd40b1
hunnyhu <huxiong888@163.com>
2018-02-05 22:31:23 +0800

    disruptor.

---
+++ commons/trunk/commons-thread/src/main/java/com/xplus/commons/thread/disruptor/getting/started/LongEvent.java
@@ -0,0 +1,14 @@
+package com.xplus.commons.thread.disruptor.getting.started;
+
+public class LongEvent {
+
+  private long value;
+
+  public long getValue() {
+    return value;
+  }
+
+  public void setValue(long value) {
+    this.value = value;
+  }
+}

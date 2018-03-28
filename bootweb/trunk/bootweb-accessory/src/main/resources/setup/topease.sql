fa781eab26dd2ccfc711ea3cb6a6fd542fcef09e
hunnyhu <huxiong888@163.com>
2018-03-28 23:17:59 +0800

    add freess.

--- bootweb/trunk/bootweb-accessory/src/main/resources/setup/topease.sql
+++ bootweb/trunk/bootweb-accessory/src/main/resources/setup/topease.sql
@@ -25,3 +25,19 @@ remark varchar(1024),
 PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- DROP TABLE freess;
+CREATE TABLE freess (
+id bigint NOT NULL AUTO_INCREMENT, 
+ip varchar(16) NOT NULL, 
+port varchar(10) NOT NULL, 
+method varchar(20) NOT NULL, 
+password varchar(250) NOT NULL, 
+vtm varchar(10) NOT NULL, -- V表示Vultr US线路，T表示China Telecom线路，M表示China Mobile线路。
+country varchar(10) NOT NULL,
+dateCreated datetime NOT NULL,
+lastUpdated datetime NOT NULL,
+INDEX index_1(ip),
+UNIQUE KEY uk_1(ip, port),
+PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
